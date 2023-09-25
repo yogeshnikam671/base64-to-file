deeca9d8f6852e5966959d70bb3c7a7ffb2b8fc9
Yogesh Nikam <yogeshnikam671@gmail.com>
2023-09-26 01:05:03 +0530

    Add support for -i, -f and -h flags

---
+++ go.mod
@@ -0,0 +1,3 @@
+module yogeshnikam671/base64-to-file
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+	"slices"
+)
+
+func createCliDirectory() (string, error) {
+  homeDir, err := os.UserHomeDir()
+  if err != nil {
+    fmt.Println("Error while getting the home directory:", err)
+    return "", err
+  }
+  cliDirectory := homeDir + "/.base64-to-file/"
+  // create a directory if it doesn't exist
+  if _, err := os.Stat(cliDirectory); os.IsNotExist(err) {
+    err := os.Mkdir(cliDirectory, 0755)
+    if err != nil {
+      fmt.Println("Error while creating a directory:", err)
+      return "", err
+    }
+  }
+  return cliDirectory, nil
+}
+
+func createFileWith(cliDirectory string) (string, error) {
+  input := os.Args[2]
+  fmt.Println("Input is:", input)
+
+  fileName := cliDirectory + "input.txt"
+  err := os.WriteFile(fileName, []byte(input), 0644) 
+  if(err != nil)  {
+    return "", err
+  }
+  return fileName, nil
+}
+
+func showHelp() {
+  fmt.Println("\nYou are seeing this due to some invalid input or you asked for help\n")
+  fmt.Println("Refer to the following help section of base64-to-file tool")
+  fmt.Println("Usage: btof [flag] [input]")
+  fmt.Println("Flags:")
+  fmt.Println("  -h, --help   shows help")
+  fmt.Println("  -i, --input <input_string>  takes input as a string")
+  fmt.Println("  -f, --file  <file_name>  takes input from a file")
+}
+
+func showHelpAndReturnTrueIfHelpShown() bool {
+  if len(os.Args) < 3 {
+    showHelp()
+    return true
+  }
+  validFlags := []string{"-h", "--help", "-i", "--input", "-f", "--file"}
+
+  flag := os.Args[1]
+  if(flag == "-h" || flag == "--help" || slices.Contains(validFlags, flag) == false) {
+    fmt.Println("\nInvalid flag:", flag, "\n")
+    showHelp()
+    return true
+  }
+  return false
+}
+
+func main() {
+  cliDirectory, err := createCliDirectory()
+  if err != nil {
+    fmt.Println("Error while creating a directory:", err)
+    return
+  }
+  isHelpShown := showHelpAndReturnTrueIfHelpShown()
+  if isHelpShown {
+    return
+  }
+
+  var fileName string
+
+  flag := os.Args[1]
+  if(flag == "-i" || flag == "--input") {
+    fileName, err = createFileWith(cliDirectory)
+    if err != nil {
+      fmt.Println("Error while processing the input:", err)
+      return
+    }
+  }
+
+  if(flag == "-f" || flag == "--file") {
+    fileName = os.Args[2]
+    fmt.Println("Reading the file:", fileName)
+  }
+
+  // read the file content and save it in a variable named input
+  inputByte, err := os.ReadFile(fileName)
+  if err != nil {
+    fmt.Println("Error while reading the file:", err)
+    return
+  }
+  input := string(inputByte)
+
+  // decode the input and store it in a byte array
+  decoded, err := base64.StdEncoding.DecodeString(input)
+  if err != nil {
+    fmt.Println("Error while decoding the string:", err)
+    return
+  }
+
+  // write to a new pdf file with the decoded content
+  decodedFileName := cliDirectory + "decoded.pdf"
+  err = os.WriteFile(decodedFileName, decoded, 0644)  // 0 110 100 100  --> 0 rwx rwx rwx
+  if err != nil {
+    fmt.Println("Error while writing the decoded content to a new file:", err)
+    return
+  }
+  fmt.Println("The file with decoded content can be found at:", decodedFileName)
+  fmt.Println("\nRun `open " + decodedFileName + "` to open the file\n")
+}
+
+
+
